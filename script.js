6d7858549674ed2f8796111d441907b70e496e97
Jake <jakepucci26@gmail.com>
2022-07-18 13:38:11 -0600

    updated to include h2

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-// code to add h2 element to the body in DOM
+h2.textContent = "Here we have a wild Sunny Bear in a rather un-natural environment. Normally, her kind would be found high in the Pyrenees mountains on the boarder of France and Spain.";
 document.querySelector("body").appendChild(h2);
-
